89feb82aad2a5335be8be5556208442efba42075
kalmtalyst <kalmtalyst@gmail.com>
2022-04-11 17:21:27 +0900

    feat: create posts and likes button for the one of them

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [title, setTitle] = useState([
+    "오트라떼 판매 커피집 모음",
+    "벚꽃놀이 2022",
+    "서울 맛집",
+  ]);
+  let [likes, setLikes] = useState(0);
+  const addLike = () => {
+    setLikes(likes + 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="nav-bar">
+        <h1>Blog</h1>
+      </section>
+      <section>
+        <div className="post-list">
+          <h3>
+            {title[0]}
+            <span onClick={addLike}> 👍 </span>
+            {likes}
+          </h3>
+          <p>3월 5일 발행</p>
+          <hr />
+        </div>
+        <div className="post-list">
+          <h3>{title[1]}</h3>
+          <p>4월 10일 발행</p>
+          <hr />
+        </div>
+        <div className="post-list">
+          <h3>{title[2]}</h3>
+          <p>4월 11일 발행</p>
+          <hr />
+        </div>
+      </section>
     </div>
   );
 }
